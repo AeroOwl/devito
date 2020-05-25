d4f9217f62b7bdc74b5cf90f61806ac62ab9a46d
George Bisbas <g.bisbas18@imperial.ac.uk>
2020-05-25 14:12:27 +0300

    nested: Add grid tests for lowering, retrieve_functions to rmv hack

--- tests/test_operator.py
+++ tests/test_operator.py
@@ -203,6 +203,54 @@ class TestCodeGen(object):
         op4 = switchconfig(language='openmp')(Operator)(Eq(u, u + 1), language='C')
         assert '#pragma omp for' not in str(op4)
 
+    def test_nested_lowering_and_indexing(self):
+        """
+        Tests that deeply nested (depth > 2) functions are lowered and indexified.
+        """
+        grid = Grid(shape=(4, 4))
+        x, y = grid.dimensions
+
+        u1 = Function(name="u1", grid=grid, dtype=np.int32)
+        u2 = Function(name="u2", grid=grid, dtype=np.int32)
+        u3 = Function(name="u3", grid=grid, dtype=np.int32)
+        u4 = Function(name="u4", grid=grid, dtype=np.int32)
+        u5 = Function(name="u5", grid=grid, dtype=np.int32)
+
+        Eq1 = Eq(u1, u2[x, u3[x, u4[x, u5[x, u1]]]])
+
+        op1 = Operator([Eq1])
+
+        op1.apply()
+
+        print(op1.ccode)
+        assert ('u1[x + 1][y + 1] = u2[x][u3[x][u4[x][u5[x]'
+                '[u1[x + 1][y + 1]]]]]') in str(op1.ccode)
+
+    def test_nested_lowering_and_indexing_rhs_lhs(self):
+        """
+        Tests that deeply nested (depth > 2) functions used as indices are indexified.
+        """
+        x1, x2, x3, x4, x5 = dimensions('x1 x2 x3 x4 x5')
+        y1, y2, y3, y4, y5 = dimensions('y1 y2 y3 y4 y5')
+
+        u1 = Function(name="u1", shape=(4, 4), dimensions=(x1, y1), dtype=np.int32)
+        u2 = Function(name="u2", shape=(4, 4), dimensions=(x2, y2), dtype=np.int32)
+        u3 = Function(name="u3", shape=(4, 4), dimensions=(x3, y3), dtype=np.int32)
+        u4 = Function(name="u4", shape=(4, 4), dimensions=(x4, y4), dtype=np.int32)
+
+        Eq1 = Eq(u1, u2[u3, u4])
+        Eq2 = Eq(u1, u2[u3[x3, y3], u4[x4, y4]])
+        op1 = Operator([Eq1])
+        op2 = Operator([Eq2])
+
+        op1.apply()
+        op2.apply()
+
+        print(op1.ccode)
+        print(op2.ccode)
+
+        assert str(op1.ccode) == str(op2.ccode)
+
     def test_nested_indexing(self):
         """
         Tests that nested functions used as indices are indexified.
